14587c1dda01a423412cc0bb422f4447e65bf71f
lihao <lihao@cmii.chinamobile.com>
2021-05-19 09:53:02 +0800

    update .gitignore

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="test-wraper">
     <h2>测试页面</h2>
+    <hr />
+    <img src="/public/favicon.ico" alt="" />
+    <hr />
+    <div class="bg-box"></div>
   </div>
 </template>
 
@@ -17,6 +21,12 @@ export default defineComponent({
   background-color: gray;
   h2 {
     color: red;
+  }
+  .bg-box {
+    width: 200px;
+    height: 200px;
+    // background-image: url('../assets/logo.png');
+    background-image: url('public/favicon.ico');
   }
 }
 </style>
