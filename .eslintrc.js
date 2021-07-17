46c04a691a1e1f8380a321cd8d13365a17a5d001
cookiepool <cookiepool@163.com>
2021-07-17 15:21:27 +0800

    chore: update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     node: true
   },
   plugins: ['vue', 'prettier'],
-  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 2020
   },
